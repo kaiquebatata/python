87292d502ee5278beaa9adccca576f58810b40e0
kaiquebatata <148916921+kaiquebatata@users.noreply.github.com>
2025-02-28 10:41:37 -0300

    Add files via upload

---
+++ Nova pasta/ex2.py
@@ -0,0 +1,25 @@
+nome1 = 'igor'
+nota1 = 9
+nome2 = 'hugo'
+nota2 = 5
+nome3 = 'isa'
+nota3 = 8
+nome4 = 'kaique'
+nota4 = 2
+nome5 = 'nicolas'
+nota5 = 6
+print ('a nota de',nome1 ,'foi igual a',nota1)
+print ('a nota de',nome2 ,'foi igual a',nota2)
+print ('a nota de',nome3 ,'foi igual a',nota3)
+print ('a nota de',nome4 ,'foi igual a',nota4)
+print ('a nota de',nome5 ,'foi igual a',nota5)
+
+name = "eu"
+note1 = 5
+note2 = 9.5
+media = (note1 + note2)/2
+disciplina = "programação de computadores"
+print ("nome do aluno",name)
+print("disciplina",disciplina)
+print("primeira nota %.2f \nsegunda nota= %.2f" %(note1,note2))
+print("media = ",media)
